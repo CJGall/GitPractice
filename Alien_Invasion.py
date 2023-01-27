74baaed3c92d5a36998b82809872247334f9f3fe
cjgall <cgalluze@gmail.com>
2023-01-27 13:54:51 -0500

    #New comments

---
+++ Alien_Invasion.py
@@ -0,0 +1,38 @@
+import sys
+
+import pygame
+
+from settings import Settings
+#new comment
+class AlienInvasion:
+    """Overall class to manager game assets and behavior"""
+
+    def __innit__(self):
+        "Initialize the game, and create game resouces"
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+        #Set the background color
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Start the main loop for the gamee"""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
+
+            #Make the most recently drawn screen invisible.
+            pygame.display.flip()
+
+
+if __name__ == '_main_':
+#make a game instance , and run this game.
+    ai = AlienInvasion()
+    ai.run_game()
+
